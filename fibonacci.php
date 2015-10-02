4ee0d4685403f3e850cbd32c85c1f23562c922dd
biroa <adam.biro@gmail.com>
2015-10-02 20:46:37 +0100

    Fibonacci solution without recursion

---
+++ fibonacci.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: biroa
+ * Date: 10/1/2015
+ * Time: 7:53 PM
+ */
+
+/**
+ * @param $n
+ */
+function fibonacci($n)
+{
+    $first = 0;
+    $second = 1;
+
+    echo "Fibonacci series ";
+    echo $first . ' ' . $second . ' ';
+
+    for ( $i = 2; $i < $n; $i++ ) {
+
+        $third = $first + $second;
+
+        echo $third . ' ';
+
+        $first = $second;
+        $second = $third;
+    }
+}
+fibonacci(10);
