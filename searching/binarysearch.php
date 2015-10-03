aedb3c5d744aa108666145580ebef05c74c72782
biroa <adam.biro@gmail.com>
2015-10-03 21:30:44 +0100

    binary search

---
+++ searching/binarysearch.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: biroa
+ * Date: 10/1/2015
+ * Time: 7:53 PM
+ *
+ * Searches for specific element in a given array using
+ * the binary search algorithm.
+ *
+ */
+
+
+/**
+ * @param array $arr
+ * @param       $value
+ * @param       $key
+ */
+function binarySearch(array $arr,$value,$key){
+
+
+}
