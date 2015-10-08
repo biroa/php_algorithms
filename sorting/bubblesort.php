add81b70fec91d7d086b183c0cc7af524492cbe9
biroa <adam.biro@gmail.com>
2015-10-08 22:41:45 +0100

    bubble sort reformat

--- sorting/bubblesort.php
+++ sorting/bubblesort.php
@@ -13,19 +13,23 @@
  *
  * @return mixed
  */
-function bubbleSort ($items) {
+function bubbleSort($items)
+{
     $size = count($items);
-    for ($i=0; $i<$size; $i++) {
-        for ($j=0; $j<$size-1-$i; $j++) {
-            if ($items[$j+1] < $items[$j]) {
-                arraySwap($items, $j, $j+1);
+    for ( $i = 0; $i < $size; $i++ ) {
+        for ( $j = 0; $j < $size - 1 - $i; $j++ ) {
+            if ( $items[$j + 1] < $items[$j] ) {
+                arraySwap($items, $j, $j + 1);
             }
         }
     }
+
     return $items;
 }
-function arraySwap (&$arr, $index1, $index2) {
-    list($arr[$index1], $arr[$index2]) = [$arr[$index2], $arr[$index1]];
+
+function arraySwap(&$arr, $index1, $index2)
+{
+    list($arr[$index1], $arr[$index2]) = [ $arr[$index2], $arr[$index1] ];
 }
 
 
