f3d12ce792d640c3dbc9bc21fe6006b40e1ddb73
biroa <adam.biro@gmail.com>
2015-10-11 11:49:10 +0100

    insertionSort

--- searching/insertionsort.php
+++ searching/insertionsort.php
@@ -7,3 +7,24 @@
  * Time: 4:34 PM
  *
  */
+
+/**
+ * @param array $numbers
+ */
+function insertionSort(array $numbers)
+{
+    $count = count($numbers);
+
+    for ( $i = 1; $i < $count; $i++ ) {
+        $j = $i - 1;
+        $key = $numbers[$i];
+        while ( $j >= 0 && $numbers[$j] > $key ) {
+            $numbers[$j + 1] = $numbers[$j];
+            $numbers[$j] = $key;
+            $j = $j - 1;
+        }
+    }
+}
+
+$numbers = [ 2, 3, 4, 5, 1, 8, 11, 0 ];
+insertionSort($numbers);
