41f51dfd73a8ee402b78ed357505b34e177a46a6
biroa <adam.biro@gmail.com>
2015-10-19 21:47:15 +0100

    binary search

--- searching/binarysearch.php
+++ searching/binarysearch.php
@@ -17,27 +17,29 @@
  *
  * @return float|int
  */
-function binarySearch($x, array $list) {
+function binarySearch($x, array $list)
+{
     $left = 0;
     $right = count($list) - 1;
 
-    while ($left <= $right) {
-        $mid = floor(($left + $right)/2);
-        echo $mid .',';
-        if ($list[$mid] == $x) {
+    while ( $left <= $right ) {
+        $mid = floor(($left + $right) / 2);
+        echo $mid . ',';
+        if ( $list[$mid] == $x ) {
             return $mid;
-        } elseif ($list[$mid] > $x) {
+        } elseif ( $list[$mid] > $x ) {
             $right = $mid - 1;
-        } elseif ($list[$mid] < $x) {
+        } elseif ( $list[$mid] < $x ) {
             $left = $mid + 1;
         }
     }
 
     return -1;
 }
-$arr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13];
+
+$arr = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 ];
 $find = 8;
 var_dump($arr);
-echo 'We look for number:'. $find .'<br>';
+echo 'We look for number:' . $find . '<br>';
 echo '<b>Binary Search with iteration: </b><br>';
-echo '<br>' . binarySearch($find,$arr);
+echo '<br>' . binarySearch($find, $arr);
