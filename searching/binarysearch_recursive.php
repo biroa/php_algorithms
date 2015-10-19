ce64916d2bb028245927e9e8058ad137d4e6ec18
biroa <adam.biro@gmail.com>
2015-10-19 21:51:57 +0100

    binary search

--- searching/binarysearch_recursive.php
+++ searching/binarysearch_recursive.php
@@ -18,24 +18,25 @@
  *
  * @return float|int
  */
-function binarySearchRecursive($x, $list, $left, $right) {
-    if ($left > $right) {
+function binarySearchRecursive($x, $list, $left, $right)
+{
+    if ( $left > $right ) {
         return -1;
     }
 
-    $mid = floor(($left + $right)/2);
-    echo $mid.' ';
-    if ($list[$mid] == $x) {
+    $mid = floor(($left + $right) / 2);
+    echo $mid . ' ';
+    if ( $list[$mid] == $x ) {
         return $mid;
-    } elseif ($list[$mid] > $x) {
+    } elseif ( $list[$mid] > $x ) {
         return binarySearchRecursive($x, $list, $left, $mid - 1);
-    } elseif ($list[$mid] < $x) {
+    } elseif ( $list[$mid] < $x ) {
         return binarySearchRecursive($x, $list, $mid + 1, $right);
     }
 }
 
 $left = 0;
 $right = 11;
-$list = [0,1,2,3,4,5,6,7,8,9,10,11];
+$list = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ];
 $x = 4;
-echo binarySearchRecursive($x,$list,$left,$right);
+echo binarySearchRecursive($x, $list, $left, $right);
